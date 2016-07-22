208a8753689e532127d18b75446f2f1e89f6fb5a
parvesh parmar <parvesh.parmar@mail.vinove.com>
2016-07-22 14:14:36 +0530

    office work

--- app/src/main/java/com/oddeven/solution/service/MainActivity.java
+++ app/src/main/java/com/oddeven/solution/service/MainActivity.java
@@ -5,14 +5,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+
+import java.security.KeyStore;
+import java.security.KeyStoreException;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+     ListView listView;
+     listAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
       //  startService(new Intent(getBaseContext(),Service_.class));
+
+        adapter=new listAdapter(getApplicationContext());
+
+        listView=(ListView)findViewById(R.id.keyList);
+        View listHeader=View.inflate(getApplicationContext(),R.layout.list_header,null);
+        listView.addHeaderView(listHeader);
+        listView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
+        try {
+            KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
 
--- app/src/main/java/com/oddeven/solution/service/listAdapter.java
+++ app/src/main/java/com/oddeven/solution/service/listAdapter.java
@@ -1,8 +1,12 @@
 package com.oddeven.solution.service;
 
+import android.content.Context;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
 
 /**
  * Created by Parvesh on 22-07-2016.
@@ -12,9 +16,20 @@ public class listAdapter extends BaseAdapter {
 
 
 
+       LayoutInflater inflater;
+     Context context;
+     listAdapter(Context context){
+             this.context=context;
+         try {
+             inflater= (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+         } catch (Exception e) {
+             e.printStackTrace();
+         }
+     }
+
     @Override
     public int getCount() {
-        return 0;
+        return 2;
     }
 
     @Override
@@ -29,6 +44,11 @@ public class listAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+
+        View view=convertView;
+
+        if(view==null)
+            view=inflater.inflate(R.layout.list_adapter,null);
+        return view;
     }
 }
